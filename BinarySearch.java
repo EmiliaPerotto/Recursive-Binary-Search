d27df47d661146ff82d78eb5c61c193a4aff6d45
Emilia Perotto <100068661@csisd.org>
2024-11-14 14:47:59 -0600

    hi

---
+++ BinarySearch.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+public class BinarySearch{
+  // public static int binaryMethod(){
+
+  // }
+  public static void main(String args[]) throws IOException{
+    Scanner in = new Scanner(System.in);
+    System.out.println("Input filename: ");
+    String filename = in.nextLine();
+    Scanner fin = new Scanner(new File(filename));
+    int count = Integer.parseInt(fin.nextLine());
+    //System.out.println(count);     this was used to check the first value
+    int[] arr = new int[count];
+    fin.nextLine();
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+      arr[i] = Integer.parseInt(fin.nextLine());
+    }
+
+
+  }
+}
